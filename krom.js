a020ab44f844a2155f30300b9590a0bd0a9d88c0
lubos <lubos.lenco@gmail.com>
2017-07-03 16:37:35 +0200

    Update Krom

--- krom.js
+++ krom.js
@@ -26,22 +26,24 @@ var indices = [0, 1, 2];
 
 Krom.init("KromApp", 640, 480, 0, false, 0);
 Krom.setCallback(renderCallback);
+Krom.setDropFilesCallback(dropFilesCallback);
 Krom.setKeyboardDownCallback(keyboardDownCallback);
 Krom.setKeyboardUpCallback(keyboardUpCallback);
+Krom.setKeyboardPressCallback(keyboardPressCallback);
 Krom.setMouseDownCallback(mouseDownCallback);
 Krom.setMouseUpCallback(mouseUpCallback);
 Krom.setMouseMoveCallback(mouseMoveCallback);
-// Krom.setMouseWheelCallback(mouseWheelCallback);
-// Krom.setGamepadAxisCallback(gamepadAxisCallback);
-// Krom.setGamepadButtonCallback(gamepadButtonCallback);
+Krom.setMouseWheelCallback(mouseWheelCallback);
+Krom.setGamepadAxisCallback(gamepadAxisCallback);
+Krom.setGamepadButtonCallback(gamepadButtonCallback);
 Krom.setAudioCallback(audioCallback);
 
-var program = Krom.createProgram();
+var pipeline = Krom.createPipeline();
 var elem = { name: "pos", data: ["Float3", 2] };
 var structure0 = { elements: [elem] };
 var vert = Krom.createVertexShaderFromSource(vs);
 var frag = Krom.createFragmentShaderFromSource(fs);
-Krom.compileProgram(program, structure0, null, null, null, 1, vert, frag, null, null, null);
+Krom.compilePipeline(pipeline, structure0, null, null, null, 1, vert, frag, null, null, null);
 
 var vb = Krom.createVertexBuffer(vertices.length / 3, structure0.elements, 0);
 var vbData = new Float32Array(vertices.length);
@@ -59,7 +61,7 @@ function renderCallback() {
 	flags |= 2; // Depth
 	Krom.clear(flags, 0xff000000, 1.0, null);
 
-	Krom.setProgram(program);
+	Krom.setPipeline(pipeline);
 	// Krom.setCullMode(0);
 	// Krom.setDepthMode(false, 0);
 	Krom.setVertexBuffer(vb);
@@ -69,8 +71,10 @@ function renderCallback() {
 	Krom.end();
 }
 
-function keyboardDownCallback(code, charCode) {}
-function keyboardUpCallback(code, charCode) {}
+function dropFilesCallback(path) {}
+function keyboardDownCallback(key) {}
+function keyboardUpCallback(key) {}
+function keyboardPressCallback(char) {}
 function mouseDownCallback(button, x, y) {}
 function mouseUpCallback(button, x, y) {}
 function mouseMoveCallback(x, y, mx, my) {}
